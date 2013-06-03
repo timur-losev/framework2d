7406515fa18893a89a737f492e3fcccb43bb1402
Timur Losev <timur.losev@gmail.com>
2013-06-03 18:12:00 +0300

    - switched to the 100th tool set - removed in-placed boost

--- qtEditor/Gui/moc_MappingToolWindow.cpp
+++ qtEditor/Gui/moc_MappingToolWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MappingToolWindow.h'
 **
-** Created: Thu 21. Mar 11:54:25 2013
+** Created: Mon 3. Jun 17:40:07 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
--- qtEditor/Gui/ui_MappingToolWindow.hpp
+++ qtEditor/Gui/ui_MappingToolWindow.hpp
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'MappingToolWindow.ui'
 **
-** Created: Thu 21. Mar 11:54:25 2013
+** Created: Mon 3. Jun 17:40:08 2013
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
